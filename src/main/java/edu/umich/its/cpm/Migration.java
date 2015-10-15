f4ec1883ad92b1c956e8aadd95fce3ba73bd97e9
Zhen Qian <zqian@umich.edu>
2015-10-15 10:10:12 -0400

    TLCPM-77 code changes after code review. Reformatted Java code for identations

--- src/main/java/edu/umich/its/cpm/Migration.java
+++ src/main/java/edu/umich/its/cpm/Migration.java
@@ -1,6 +1,5 @@
 package edu.umich.its.cpm;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,77 +21,63 @@ import lombok.Setter;
 @Entity
 public class Migration {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="MIGRATION_ID",
-	table="MIGRATION")
-	@Getter @Setter
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "MIGRATION_ID", table = "MIGRATION")
+	@Getter
+	@Setter
 	private Integer migration_id;
-	
-	@Column(name="SITE_ID",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String site_id; 
-	
-	@Column(name="SITE_NAME",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String site_name; 
-	
-	@Column(name="TOOL_NAME",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String tool_name;
-	
-	@Column(name="TOOL_ID",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String tool_id; 
-	
-	@Column(name="MIGRATED_BY",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String migrated_by;
-	
-	@Column(name="START",
-	columnDefinition="TIMESTAMP NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private Timestamp start;
-	
-	@Column(name="END",
-	columnDefinition="TIMESTAMP",
-	table="MIGRATION")
-	@Getter @Setter
-	private Timestamp end;
-	
-	@Column(name="DESTINATION_TYPE",
-	columnDefinition="VARCHAR(99) NOT NULL",
-	table="MIGRATION")
-	@Getter @Setter
-	private String destination_type;
-	
-	@Column(name="DESTINATION_URL",
-	columnDefinition="VARCHAR(99)",
-	table="MIGRATION")
-	@Getter @Setter
-	private String destination_url;
-	
-	protected Migration() {}
 
-	public Migration(String site_id,
-					String site_name,
-					String tool_id,
-					String tool_name,
-					String migrated_by,
-					Timestamp start,
-					Timestamp end,
-					String destination_type,
-					String destination_url) {
+	@Column(name = "SITE_ID", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String site_id;
+
+	@Column(name = "SITE_NAME", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String site_name;
+
+	@Column(name = "TOOL_NAME", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String tool_name;
+
+	@Column(name = "TOOL_ID", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String tool_id;
+
+	@Column(name = "MIGRATED_BY", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String migrated_by;
+
+	@Column(name = "START", columnDefinition = "TIMESTAMP NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private Timestamp start;
+
+	@Column(name = "END", columnDefinition = "TIMESTAMP", table = "MIGRATION")
+	@Getter
+	@Setter
+	private Timestamp end;
+
+	@Column(name = "DESTINATION_TYPE", columnDefinition = "VARCHAR(99) NOT NULL", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String destination_type;
+
+	@Column(name = "DESTINATION_URL", columnDefinition = "VARCHAR(99)", table = "MIGRATION")
+	@Getter
+	@Setter
+	private String destination_url;
+
+	protected Migration() {
+	}
+
+	public Migration(String site_id, String site_name, String tool_id,
+			String tool_name, String migrated_by, Timestamp start,
+			Timestamp end, String destination_type, String destination_url) {
 		this.site_id = site_id;
 		this.site_name = site_name;
 		this.tool_id = tool_id;
@@ -106,18 +91,11 @@ public class Migration {
 
 	@Override
 	public String toString() {
-		return String.format(
-					"Migration[migration_id=%d, "
-					+ "site_id='%s',"
-					+ "site_name='%s',"
-					+ "tool_id='%s',"
-					+ "tool_name='%s',"
-					+ "migrated_by='%s',"
-					+ "start='%s',"
-					+ "end='%s',"
-					+ "destination_type='%s',"
-					+ "destination_url='%s'"
-					+ "]",
-					migration_id, site_id,site_name, tool_id, tool_name, migrated_by, start, end, destination_type, destination_url);
+		return String.format("Migration[migration_id=%d, " + "site_id='%s',"
+				+ "site_name='%s'," + "tool_id='%s'," + "tool_name='%s',"
+				+ "migrated_by='%s'," + "start='%s'," + "end='%s',"
+				+ "destination_type='%s'," + "destination_url='%s'" + "]",
+				migration_id, site_id, site_name, tool_id, tool_name,
+				migrated_by, start, end, destination_type, destination_url);
 	}
 }
--- src/main/java/edu/umich/its/cpm/MigrationController.java
+++ src/main/java/edu/umich/its/cpm/MigrationController.java
@@ -29,168 +29,221 @@ import org.json.JSONArray;
 @RestController
 public class MigrationController {
 
-	private static final Logger log = LoggerFactory.getLogger(MigrationController.class);
-	
-    private final AtomicLong counter = new AtomicLong();
-    
-    @Autowired MigrationRepository repository;
-    
-    @Autowired
-    private Environment env;
-    
-    /**
-     * get all CTools sites where user have site.upd permission 
-     * @return
-     */
-    @RequestMapping("/projects")
-    public String getProjectSites() {
-    	String rv = "";
-    	
-    	// login to CTools and get sessionId
-    	String sessionId = login_becomeuser();
-    	if (sessionId != null)
-    	{
+	private static final Logger log = LoggerFactory
+			.getLogger(MigrationController.class);
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@Autowired
+	MigrationRepository repository;
+
+	@Autowired
+	private Environment env;
+
+	/**
+	 * get all CTools sites where user have site.upd permission
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/projects")
+	public String getProjectSites(HttpServletRequest request) {
+		String rv = "";
+
+		// login to CTools and get sessionId
+		String sessionId = login_becomeuser(request);
+		if (sessionId != null) {
 			// get all sites that user have permission site.upd
 			RestTemplate restTemplate = new RestTemplate();
-			// the url should be in the format of "https://server/direct/site/withPerm/.json?permission=site.upd"
-			String requestUrl = env.getProperty("ctools.direct.url") + "site/withPerm/.json?permission=site.upd&_sessionId=" + sessionId;
+			// the url should be in the format of
+			// "https://server/direct/site/withPerm/.json?permission=site.upd"
+			String requestUrl = env.getProperty("ctools.direct.url")
+					+ "site/withPerm/.json?permission=site.upd&_sessionId="
+					+ sessionId;
 			log.info(requestUrl);
-        	try
-        	{
-        		rv= restTemplate.getForObject(requestUrl, String.class);
-        	}
-        	catch (RestClientException e)
-        	{
-        		log.error(requestUrl + e.getMessage());
-        	}
-        }
-        return rv;
-    }
-    
-    /**
-     * get page information
-     * @param site_id
-     * @return
-     */
-    @RequestMapping("/projects/{site_id}")
-    public String getProjectSitePages(@PathVariable String site_id) {
-    	String rv = "";
-    	
-    	// login to CTools and get sessionId
-		String sessionId = login_becomeuser();
-		if (sessionId != null)
-		{	
-        	//3. get all sites that user have permission site.upd
-        	RestTemplate restTemplate = new RestTemplate();
-        	// the url should be in the format of "https://server/direct/site/SITE_ID.json"
-        	String requestUrl = env.getProperty("ctools.direct.url") + "site/" + site_id + "/pages.json?_sessionId=" + sessionId;
-        	log.info(requestUrl);
-        	
-        	try
-        	{
-        		rv= restTemplate.getForObject(requestUrl, String.class);
-        	}
-			catch (RestClientException e)
-			{
+			try {
+				rv = restTemplate.getForObject(requestUrl, String.class);
+			} catch (RestClientException e) {
 				log.error(requestUrl + e.getMessage());
 			}
-        }
+		}
 		return rv;
-    }
+	}
+
+	/**
+	 * get page information
+	 * 
+	 * @param site_id
+	 * @return
+	 */
+	@RequestMapping("/projects/{site_id}")
+	public String getProjectSitePages(@PathVariable String site_id,
+			HttpServletRequest request) {
+		String rv = "";
+
+		// login to CTools and get sessionId
+		String sessionId = login_becomeuser(request);
+		if (sessionId != null) {
+			// 3. get all sites that user have permission site.upd
+			RestTemplate restTemplate = new RestTemplate();
+			// the url should be in the format of
+			// "https://server/direct/site/SITE_ID.json"
+			String requestUrl = env.getProperty("ctools.direct.url") 
+					+ "site/"
+					+ site_id 
+					+ "/pages.json?_sessionId=" 
+					+ sessionId;
+			log.info(requestUrl);
+
+			try {
+				rv = restTemplate.getForObject(requestUrl, String.class);
+			} catch (RestClientException e) {
+				log.error(requestUrl + e.getMessage());
+				rv = "Cannot find site by siteId: " + site_id;
+			}
+		}
+		return rv;
+	}
 
 	/**
 	 * login into CTools and become user with sessionId
 	 */
-	private String login_becomeuser() {
+	private String login_becomeuser(HttpServletRequest request) {
 		// return the session id after login
 		String sessionId = "";
-		
-		// here is the CTools integration prior to CoSign integration ( read session user information from configuration file)
-    	// 1. create a session based on user id and password
-    	RestTemplate restTemplate = new RestTemplate();
-    	// the url should be in the format of "https://server/direct/session?_username=USERNAME&_password=PASSWORD"
-    	String requestUrl = env.getProperty("ctools.direct.url") + "session?_username=" + env.getProperty("username") + "&_password=" + env.getProperty("password");
-    	ResponseEntity<String> response = restTemplate.postForEntity(requestUrl, null, String.class);
-        HttpStatus status = response.getStatusCode();
-        
-       if (status.value() != 201)
-        {
-        	// return error if a new CTools session could not be created using username and password provided
-        	log.info("Wrong user id or password. Cannot login to CTools " + env.getProperty("ctools.direct.url"));
-        }
-        else
-        {
-        	// get the session id
-            sessionId = response.getBody();
-            log.info("successfully logged in as user " + env.getProperty("username") + " with sessionId = " + sessionId);
-            
-        	// 2. become the user
-        	// retrieve from the configuration file for now; Shoudl get from the REMOTE_USER setting after CoSign integration
-        	restTemplate = new RestTemplate();
-        	// the url should be in the format of "https://server/direct/session/SESSION_ID.json"
-        	requestUrl = env.getProperty("ctools.direct.url") + "session/becomeuser/" + env.getProperty("become.username")+".json?_sessionId=" + sessionId;
-        	log.info(requestUrl);
 
-        	try
-        	{
-        		String resultString = restTemplate.getForObject(requestUrl, String.class, sessionId); 
-        		log.info(resultString);
-        	}
-        	catch (RestClientException e)
-        	{
-        		log.error(requestUrl + e.getMessage());
-        		
-        		// nullify sessionId if become user call is not successful
-        		sessionId = null;
-        	}
-        }
-        return sessionId;
-    }
+		String remoteUser = request.getRemoteUser();
+		log.info("remote user is " + remoteUser);
+		// here is the CTools integration prior to CoSign integration ( read
+		// session user information from configuration file)
+		// 1. create a session based on user id and password
+		RestTemplate restTemplate = new RestTemplate();
+		// the url should be in the format of
+		// "https://server/direct/session?_username=USERNAME&_password=PASSWORD"
+		String requestUrl = env.getProperty("ctools.direct.url")
+				+ "session?_username=" 
+				+ env.getProperty("username")
+				+ "&_password=" 
+				+ env.getProperty("password");
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				requestUrl, null, String.class);
+		HttpStatus status = response.getStatusCode();
 
-    /**
-     * get all migration records
-     * @return
-     */
-    @RequestMapping("/migrations")
-    public String migrations() {
-    	return JSONObject.valueToString(repository.findAll());
-    }
-    
-    /**
-     * get a specific migration record
-     * @param migration_id
-     * @return
-     */
-    @RequestMapping("/migrations/{migration_id}")
-    public String migrations(@PathVariable("migration_id") Integer migration_id) {
-    	return JSONObject.valueToString(repository.findOne(migration_id));
-    }
-    
-    /**
-     * found all migrated records (where the migration record have "end" field value
-     * @return
-     */
-    @RequestMapping("/migrated")
-    public String migrated() {
-    	return JSONObject.valueToString(repository.findMigrated());
-    }
-    
-    /**
-     * insert a new record of Migration
-     * @param request
-     */
-    @RequestMapping(value="/migration", method = RequestMethod.POST)
-    public void migrated(HttpServletRequest request) {
-	    Map<String, String[]> parameterMap = request.getParameterMap();
-	    Migration m = new Migration(parameterMap.get("site_id")[0],
-	    		parameterMap.get("site_name")[0],
-	    		parameterMap.get("tool_id")[0],
-	    		parameterMap.get("tool_name")[0],
-	    		parameterMap.get("migrated_by")[0],
-	    		new java.sql.Timestamp(System.currentTimeMillis()), /* start time is now*/
+		if (!status.equals(HttpStatus.CREATED)) {
+			// return error if a new CTools session could not be created using
+			// username and password provided
+			log.info("Wrong user id or password. Cannot login to CTools "
+					+ env.getProperty("ctools.direct.url"));
+		} else {
+			// get the session id
+			sessionId = response.getBody();
+			log.info("successfully logged in as user "
+					+ env.getProperty("username") 
+					+ " with sessionId = "
+					+ sessionId);
+
+			// 2. become the user
+			// retrieve from the configuration file for now; Shoudl get from the
+			// REMOTE_USER setting after CoSign integration
+			restTemplate = new RestTemplate();
+			// the url should be in the format of
+			// "https://server/direct/session/SESSION_ID.json"
+			requestUrl = env.getProperty("ctools.direct.url")
+					+ "session/becomeuser/" 
+					+ remoteUser 
+					+ ".json?_sessionId="
+					+ sessionId;
+			log.info(requestUrl);
+
+			try {
+				String resultString = restTemplate.getForObject(requestUrl,
+						String.class, sessionId);
+				log.info(resultString);
+			} catch (RestClientException e) {
+				log.error(requestUrl + e.getMessage());
+
+				// nullify sessionId if become user call is not successful
+				sessionId = null;
+			}
+		}
+		return sessionId;
+	}
+
+	/**
+	 * get all migration records
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/migrations")
+	public String migrations() {
+		return JSONObject.valueToString(repository.findAll());
+	}
+
+	/**
+	 * get a specific migration record
+	 * 
+	 * @param migration_id
+	 * @return
+	 */
+	@RequestMapping("/migrations/{migration_id}")
+	public String migrations(@PathVariable("migration_id") Integer migration_id) {
+		Object o = repository.findOne(migration_id);
+		if (o != null) {
+			// find migration record with id
+			return JSONObject.valueToString(o);
+		} else {
+			// no such migration record
+			return "no matching record for /migrations/" + migration_id;
+		}
+	}
+
+	/**
+	 * found all migrated records (where the migration record have "end" field
+	 * value
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/migrated")
+	public String migrated() {
+		return JSONObject.valueToString(repository.findMigrated());
+	}
+
+	/**
+	 * insert a new record of Migration
+	 * 
+	 * @param request
+	 */
+	@RequestMapping(value = "/migration", method = RequestMethod.POST)
+	public Migration migrated(HttpServletRequest request) {
+		Map<String, String[]> parameterMap = request.getParameterMap();
+		Migration m = new Migration(parameterMap.get("site_id")[0],
+				parameterMap.get("site_name")[0],
+				parameterMap.get("tool_id")[0],
+				parameterMap.get("tool_name")[0],
+				parameterMap.get("migrated_by")[0], new java.sql.Timestamp(
+						System.currentTimeMillis()), /* start time is now */
 				null, /* no end time */
-				parameterMap.get("destination_type")[0],
-				null);
-	    repository.save(m);
-    }
+				parameterMap.get("destination_type")[0], null);
+		Migration rv = null;
+		try {
+			rv = repository.save(m);
+		} catch (Exception e) {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("Exception saving migration record site_id=")
+					.append(parameterMap.get("site_id")[0])
+					.append(" site_name=")
+					.append(parameterMap.get("site_name")[0])
+					.append(" tool_id=")
+					.append(parameterMap.get("tool_id")[0])
+					.append(" tool_name=")
+					.append(parameterMap.get("tool_name")[0])
+					.append(" migrated_by=")
+					.append(parameterMap.get("migrated_by")[0])
+					.append(" destination_type=")
+					.append(parameterMap.get("destination_type")[0])
+					.append(" \n ")
+					.append(e.getMessage());
+			log.error(buffer.toString());
+		}
+		return rv;
+	}
 }
