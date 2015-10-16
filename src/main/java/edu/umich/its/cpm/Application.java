adbf0cd78f8af0baa766ad319122cb7a57d207b0
Zhen Qian <zqian@umich.edu>
2015-10-16 10:42:59 -0400

    TLCPM-90 missed changes in Application.java

--- src/main/java/edu/umich/its/cpm/Application.java
+++ src/main/java/edu/umich/its/cpm/Application.java
@@ -7,17 +7,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import java.sql.Timestamp;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	private static final Logger log = LoggerFactory
 			.getLogger(Application.class);
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
