bdb1da07674992075bfad96c996f02d2122b54f3
Zhen Qian <zqian@umich.edu>
2015-10-23 11:01:48 -0400

    TLCPM-92 get Box item list json feed

--- src/main/java/edu/umich/its/cpm/MigrationController.java
+++ src/main/java/edu/umich/its/cpm/MigrationController.java
@@ -8,10 +8,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.Map;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,13 +33,18 @@ import org.json.JSONArray;
 
 import com.box.sdk.BoxAPIConnection;
 import com.box.sdk.BoxFolder;
+import com.box.sdk.BoxFile;
 import com.box.sdk.BoxItem;
+import com.box.sdk.BoxItem.Info;
 import com.box.sdk.BoxUser;
 
 import com.google.gson.Gson;
 
 @RestController
 public class MigrationController {
+
+	private static final SimpleDateFormat date_formatter = new SimpleDateFormat(
+			"yyyy-MM-dd-hh.mm.ss");
 
 	private static final Logger log = LoggerFactory
 			.getLogger(MigrationController.class);
@@ -96,11 +104,8 @@ public class MigrationController {
 			RestTemplate restTemplate = new RestTemplate();
 			// the url should be in the format of
 			// "https://server/direct/site/SITE_ID.json"
-			String requestUrl = env.getProperty("ctools.direct.url") 
-					+ "site/"
-					+ site_id 
-					+ "/pages.json?_sessionId=" 
-					+ sessionId;
+			String requestUrl = env.getProperty("ctools.direct.url") + "site/"
+					+ site_id + "/pages.json?_sessionId=" + sessionId;
 			log.info(requestUrl);
 
 			try {
@@ -129,10 +134,9 @@ public class MigrationController {
 		// the url should be in the format of
 		// "https://server/direct/session?_username=USERNAME&_password=PASSWORD"
 		String requestUrl = env.getProperty("ctools.direct.url")
-				+ "session?_username=" 
-				+ env.getProperty("username")
-				+ "&_password=" 
-				+ env.getProperty("password");
+				+ "session?_username=" + env.getProperty("username")
+				+ "&_password=" + env.getProperty("password");
+		log.info(requestUrl);
 		ResponseEntity<String> response = restTemplate.postForEntity(
 				requestUrl, null, String.class);
 		HttpStatus status = response.getStatusCode();
@@ -146,18 +150,16 @@ public class MigrationController {
 			// get the session id
 			sessionId = response.getBody();
 			log.info("successfully logged in as user "
-					+ env.getProperty("username") 
-					+ " with sessionId = "
+					+ env.getProperty("username") + " with sessionId = "
 					+ sessionId);
 
-			// 2. become the user based on REMOTE_USER setting after CoSign integration
+			// 2. become the user based on REMOTE_USER setting after CoSign
+			// integration
 			restTemplate = new RestTemplate();
 			// the url should be in the format of
 			// "https://server/direct/session/SESSION_ID.json"
 			requestUrl = env.getProperty("ctools.direct.url")
-					+ "session/becomeuser/" 
-					+ remoteUser 
-					+ ".json?_sessionId="
+					+ "session/becomeuser/" + remoteUser + ".json?_sessionId="
 					+ sessionId;
 			log.info(requestUrl);
 
@@ -225,10 +227,9 @@ public class MigrationController {
 		Migration m = new Migration(parameterMap.get("site_id")[0],
 				parameterMap.get("site_name")[0],
 				parameterMap.get("tool_id")[0],
-				parameterMap.get("tool_name")[0],
-				request.getRemoteUser(),
-				new java.sql.Timestamp(System.currentTimeMillis()), /* start time is now */
-				null, /* no end time */
+				parameterMap.get("tool_name")[0], request.getRemoteUser(),
+				new java.sql.Timestamp(System.currentTimeMillis()), // start time is now
+				null, // no end time
 				parameterMap.get("destination_type")[0], null);
 		Migration rv = null;
 		try {
@@ -239,77 +240,141 @@ public class MigrationController {
 					.append(parameterMap.get("site_id")[0])
 					.append(" site_name=")
 					.append(parameterMap.get("site_name")[0])
-					.append(" tool_id=")
-					.append(parameterMap.get("tool_id")[0])
+					.append(" tool_id=").append(parameterMap.get("tool_id")[0])
 					.append(" tool_name=")
 					.append(parameterMap.get("tool_name")[0])
-					.append(" migrated_by=")
-					.append(request.getRemoteUser())
+					.append(" migrated_by=").append(request.getRemoteUser())
 					.append(" destination_type=")
 					.append(parameterMap.get("destination_type")[0])
-					.append(" \n ")
-					.append(e.getMessage());
+					.append(" \n ").append(e.getMessage());
 			log.error(buffer.toString());
 		}
 		return rv;
 	}
-	
+
 	/************* Box integration *****************/
 	/**
 	 * get json string og box folders
+	 * 
 	 * @return
 	 */
 	private String DEVELOPER_TOKEN = "";
 	private static final int MAX_DEPTH = 1;
-    
+
 	@RequestMapping("/box/folders")
 	public String getBoxFolders() {
-		
+
 		String rv = "";
 
-		// TODO: get the DEVELOPER_TOKEN from configuratio for now, will do token auto-generation in the future
+		// TODO: get the DEVELOPER_TOKEN from configuratio for now, will do
+		// token auto-generation in the future
 		DEVELOPER_TOKEN = env.getProperty("box_api_token");
-		
+
+		// log in
 		BoxAPIConnection api = new BoxAPIConnection(DEVELOPER_TOKEN);
 		BoxUser.Info userInfo = BoxUser.getCurrentUser(api).getInfo();
-		log.info("Box user log in success: user name = " + userInfo.getName() + " user login = " + userInfo.getLogin());
-		
+		log.info("Box user log in success: user name = " + userInfo.getName()
+				+ " user login = " + userInfo.getLogin());
+
+		// get the root Box folder
 		BoxFolder rootFolder = BoxFolder.getRootFolder(api);
-		List<BoxFolder> folders = listFolder(rootFolder, 0);
-		
-		// create a new Gson instance
-		 Gson gson = new Gson();
-		 // convert BoxFolder list to json
-		 rv = gson.toJson(folders);
-		
+
+		// get list of properties from all Box items contained
+		List<HashMap<String, String>> folderItems = listFolderAndFile(null,
+				api, rootFolder, "");
+
+		// construct JSON
+		Gson gson = new Gson();
+		rv = gson.toJson(folderItems);
+
 		return rv;
 	}
-	
-	/**
-	 * get list of BoxFolder object recursively
-	 * @param folder
-	 * @param depth
-	 * @return
-	 */
-	private static List<BoxFolder> listFolder(BoxFolder folder, int depth) {
-		List<BoxFolder> rv = new ArrayList<BoxFolder>();
-		
-		for (BoxItem.Info itemInfo : folder) {
-			String indent = "";
-			for (int i = 0; i < depth; i++) {
-				indent += "    ";
-			}
 
-			log.info(indent + itemInfo.getName());
-			if (itemInfo instanceof BoxFolder.Info) {
-				BoxFolder childFolder = (BoxFolder) itemInfo.getResource();
-				rv.add(childFolder);
-				if (depth < MAX_DEPTH) {
-					listFolder(childFolder, depth + 1);
-				}
+	/**
+	 * recursively get all file and folder items inside the root folder
+	 */
+	public List<HashMap<String, String>> listFolderAndFile(
+			List<HashMap<String, String>> rv, BoxAPIConnection api,
+			BoxFolder folder, String folderPath) {
+		if (rv == null)
+			rv = new ArrayList<HashMap<String, String>>();
+
+		for (BoxItem.Info itemInfo : folder) {
+
+			if (itemInfo instanceof BoxFile.Info) {
+
+				BoxFile.Info fileInfo = (BoxFile.Info) itemInfo;
+				BoxFile file = new BoxFile(api, fileInfo.getID());
+
+				// construct path
+				String filePath = folderPath + "/" + fileInfo.getName();
+
+				// get item properties
+				rv.add(getBoxItemProperties(file.getInfo(), filePath));
+
+			} else if (itemInfo instanceof BoxFolder.Info) {
+
+				BoxFolder.Info folderInfo = (BoxFolder.Info) itemInfo;
+				BoxFolder xfolder = new BoxFolder(api, folderInfo.getID());
+				String currentFolderPath = folderPath + "/"
+						+ folderInfo.getName();
+				rv.add(getBoxItemProperties(xfolder.getInfo(),
+						currentFolderPath));
+				listFolderAndFile(rv, api, xfolder, currentFolderPath);
 			}
 		}
-		
+
+		return rv;
+
+	}
+
+	/**
+	 * get BoxItem properties
+	 */
+	private HashMap<String, String> getBoxItemProperties(BoxItem.Info info,
+			String path) {
+		HashMap<String, String> rv = new HashMap<String, String>();
+		rv.put("name", info.getName());
+		rv.put("size", String.valueOf(info.getSize()));
+
+		// type defaults to folder
+		String type = "folder";
+		if (info instanceof BoxFile.Info) {
+			rv.put("sh1", ((BoxFile.Info) info).getSha1());
+			type = "file";
+		}
+		rv.put("type", type);
+
+		rv.put("content_created_at", format_date(info.getContentCreatedAt()));
+		rv.put("content_modified_at", format_date(info.getContentModifiedAt()));
+		rv.put("created_at", format_date(info.getCreatedAt()));
+		rv.put("created_by", get_box_user_name(info.getCreatedBy()));
+		rv.put("modified_by", get_box_user_name(info.getCreatedBy()));
+		rv.put("owned_by", get_box_user_name(info.getOwnedBy()));
+		rv.put("description", info.getDescription());
+		rv.put("path", path);
+		return rv;
+	}
+
+	/**
+	 * a function to format Date object
+	 */
+	private String format_date(Date date) {
+		String rv = "";
+		if (date != null) {
+			rv = date_formatter.format(date);
+		}
+		return rv;
+	}
+
+	/**
+	 * return the name of BoxUser
+	 */
+	private String get_box_user_name(BoxUser.Info uInfo) {
+		String rv = "";
+		if (uInfo != null) {
+			rv = uInfo.getName();
+		}
 		return rv;
 	}
 }
