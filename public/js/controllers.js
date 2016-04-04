74047aed660860a80f9dbcb81ba332d0a52a6e77
Gonzalo Silverio <gsilver@umich.edu>
2016-04-04 09:42:56 -0400

    TLCPM-216 - sort current migrations as required

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -43,7 +43,7 @@ projectMigrationApp.controller('projectMigrationController', ['Projects', 'Migra
       $log.info(' - - - - GET /migrating');
       
       if (result.data.entity.length && result.status ===200) {
-        $scope.migratingProjects = _.sortBy(transformMigrations(result).data.entity, 'start_time');
+        $scope.migratingProjects = _.sortBy(transformMigrations(result).data.entity, 'start_time').reverse();
         $log.info('Updating projects panel based on CURRENT migrations');
         updateProjectsPanel($scope.migratingProjects, 'migrating');
       }
@@ -388,13 +388,13 @@ var poll = function (pollName, url, interval, targetPanel){
         }
         //update time stamp displayed in /migrations panel
         $rootScope.status.migrations = moment().format('h:mm:ss');
-        $scope.migratingProjects = _.sortBy(transformMigrations(result).data.entity, 'start_time');
+        $scope.migratingProjects = _.sortBy(transformMigrations(result).data.entity, 'start_time').reverse();
         // this poll has different data than the last one
         if(!angular.equals($scope.migratingProjects, $scope.migratingProjectsShadow)){
           $log.info('Updating projects panel based on CURRENT migrations poll');
           updateProjectsPanel($scope.migratingProjects, 'migrating');
         }
-        $scope.migratingProjectsShadow = _.sortBy(transformMigrations(result).data.entity, 'site_id');
+        $scope.migratingProjectsShadow = _.sortBy(transformMigrations(result).data.entity, 'start_time').reverse();
         //clear error condition if any
         $scope.migratingProjectsError = false;
       }
