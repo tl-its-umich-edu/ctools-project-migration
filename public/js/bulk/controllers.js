89aa4186cef5f4e6721eb0331f10c8c04011bf99
Gonzalo Silverio <gsilver@umich.edu>
2016-05-19 14:14:17 -0400

    TLCPM-285 - rearranging js file structure

---
+++ public/js/bulk/controllers.js
@@ -0,0 +1,17 @@
+'use strict';
+/* global projectMigrationApp*/
+
+projectMigrationApp.controller('projectMigrationBatchController', [
+    '$rootScope', '$scope', '$log', '$q', '$window', 'BulkUpload',
+    function($rootScope, $scope, $log, $q, $window, BulkUpload) {
+
+        $scope.bulkUpload = function() {
+            var file = $scope.bulkUploadFile;
+            var bulkUploadUrl = $rootScope.urls.bulkUploadUrl;
+            BulkUpload.bulkUpload(file, bulkUploadUrl).then(function() {
+                $log.info('hhh after ' + bulkUploadUrl);
+                // TODO
+            });
+        };
+    }
+]);
---
+++ public/js/directives.js
@@ -0,0 +1,18 @@
+'use strict';
+/* global projectMigrationApp */
+
+// getting the file-model attribute into controller's scope
+projectMigrationApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
