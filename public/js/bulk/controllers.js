abd9a29d51eba9557ced1809c3fceff878320782
Gonzalo Silverio <gsilver@umich.edu>
2016-05-20 09:25:23 -0400

    TLCPM-285 - fix bulkUpload post url, make app.js an actual js file

--- public/js/bulk/controllers.js
+++ public/js/bulk/controllers.js
@@ -5,7 +5,7 @@ projectMigrationApp.controller('projectMigrationBatchController', ['$rootScope',
   function($rootScope, $scope, $log, $q, $window, BulkUpload) {
     $scope.bulkUpload = function() {
       var file = $scope.bulkUploadFile;
-      var bulkUploadUrl = $rootScope.urls.bulkUploadUrl;
+      var bulkUploadUrl = $rootScope.urls.bulkUploadPostUrl;
       BulkUpload.bulkUpload(file, bulkUploadUrl).then(function() {
         $log.info('hhh after ' + bulkUploadUrl);
         // TODO
