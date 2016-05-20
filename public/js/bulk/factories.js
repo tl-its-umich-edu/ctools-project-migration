54d3ae0b7a2063809de55d126eaea30aabb068a8
Gonzalo Silverio <gsilver@umich.edu>
2016-05-20 07:35:25 -0400

    TLCPM-285 - reformat, lint, add init vals for bulk app, stubs

--- public/js/bulk/factories.js
+++ public/js/bulk/factories.js
@@ -7,18 +7,18 @@ projectMigrationApp.factory('BulkUpload', function($http, $log) {
     bulkUpload: function(file, uploadUrl) {
       $log.info("uploadUrl" + uploadUrl);
       var fd = new FormData();
-          fd.append('file', file);
-          return $http.post(uploadUrl, fd, {
-              transformRequest: angular.identity,
-              headers: {'Content-Type': undefined}
-          }).then(
+      fd.append('file', file);
+      return $http.post(uploadUrl, fd, {
+        transformRequest: angular.identity,
+        headers: { 'Content-Type': undefined }
+      }).then(
         function success(result) {
           // forward the data - let the controller deal with it
           return result;
         },
         function error(result) {
           errorDisplay(result.status,
-              'Unable to post new migration');
+            'Unable to post new migration');
           result.errors.failure = true;
           return result;
         });
