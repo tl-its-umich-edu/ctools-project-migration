b08ddb93593aa633ff9f3cffc27fd736de213567
Silverio <gsilver@umich.edu>
2016-05-23 16:15:50 -0400

    TLCPM-286-2 - use ongoing and concluded feeds to populate ui, stubs or live

--- public/js/bulk/controllers.js
+++ public/js/bulk/controllers.js
@@ -11,5 +11,34 @@ projectMigrationApp.controller('projectMigrationBatchController', ['$rootScope',
         // TODO
       });
     };
+
+    $scope.getOngoingList = function() {
+      var listBulkUploadOngoingUrl = $rootScope.urls.listBulkUploadOngoingUrl;
+      BulkUpload.getList(listBulkUploadOngoingUrl).then(function(resultOngoing) {
+        $log.info('Getting ongoing batches with  ' + listBulkUploadOngoingUrl);
+        $scope.ongoing =resultOngoing.data.entity;
+      });
+    };
+    $scope.getConcludedList = function() {
+      var listBulkUploadConcludedUrl = $rootScope.urls.listBulkUploadConcludedUrl;
+      BulkUpload.getList(listBulkUploadConcludedUrl).then(function(resultConcluded) {
+        $log.info('Getting concluded batches with  ' + listBulkUploadConcludedUrl);
+        $scope.concluded =resultConcluded.data.entity;
+      });
+    };
+    $scope.getUploadList = function(batchId, $index) {
+      // non stub version
+      var bulkUploadListUrl = $rootScope.urls.bulkUploadPostUrl + '/' + batchId;
+      if ($rootScope.stubs){
+        // stub version
+        bulkUploadListUrl = $rootScope.urls.bulkUploadList;
+      }
+      BulkUpload.getList(bulkUploadListUrl).then(function(resultList) {
+        $log.info('Getting list of sites in a batch process  batches with  ' + bulkUploadListUrl);
+        $scope.ongoing[$index].list = resultList.data.entity;
+      });
+      return null;
+    };
+    $scope.getOngoingList();
   }
 ]);
