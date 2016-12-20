25160cc9f52ac8c55961e1c9e2312e3fc1ce6035
pushyami <pushyami@umich.edu>
2016-12-20 08:40:03 -0500

    TLCPM-588 changing  the folder name format for zip mail download

--- src/main/java/edu/umich/its/cpm/MigrationTaskService.java
+++ src/main/java/edu/umich/its/cpm/MigrationTaskService.java
@@ -1660,7 +1660,7 @@ class MigrationTaskService {
 			{
 				messageFolderName = channelName + Utils.PATH_SEPARATOR;
 			}
-			String name = messageFolderName + " " + sender + " " + date + " " + subject;
+			String name = messageFolderName + " " + date + " " + sender + " " + subject;
 			if(name.length()>100){
 				name=name.substring(0,100);
 			}
