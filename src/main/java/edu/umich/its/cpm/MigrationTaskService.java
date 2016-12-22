baf540b0104a9eb12d0324a716f4456763fd4248
Zhen Qian <zqian@umich.edu>
2016-12-22 15:45:33 -0500

    Merge pull request #370 from zqian/TLCPM-604
    TLCPM-604 catch all BoxAPIException and add error message to the item…

--- src/main/java/edu/umich/its/cpm/MigrationTaskService.java
+++ src/main/java/edu/umich/its/cpm/MigrationTaskService.java
@@ -10,6 +10,7 @@ import org.apache.http.protocol.HttpContext;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -701,22 +702,28 @@ class MigrationTaskService {
 							log.debug(Utils.PARAM_BOX_FOLDER_ID_STACK + " length="
 									+ boxFolderIdStack.size());
 						} catch (BoxAPIException e) {
-							log.error(this + " boxUploadSiteContent "
-									+ e.getResponse());
-							JSONObject eJSON = new JSONObject(e.getResponse());
-							String errorMessage = eJSON.has("context_info") ? eJSON
-									.getString("context_info") : "";
-									itemStatus
-									.append("Box upload process was stopped due to the following error. Please rename the folder/resource item and migrate site again: \""
-											+ errorMessage + "\"");
-									// the status of file upload to Box
-									MigrationFileItem item = new MigrationFileItem(
-											contentUrl, title, itemStatus.toString());
-									rv.add(item);
+							String errorMessage = "There is a problem uploading item " + title + " to Box: "
+									+ e.getMessage();
+							try
+							{
+								JSONObject eJSON = new JSONObject(e.getResponse());
+								errorMessage = errorMessage.concat(eJSON.has("context_info")?eJSON.getString("context_info"):"");
+							}
+							catch (JSONException ee)
+							{
+								log.error("Cannot parse JSONObject out of " + e.getResponse());
+							}
+							
+							log.error(this + errorMessage);
+							
+							// the status of file upload to Box
+							itemStatus.append(errorMessage);
+							MigrationFileItem item = new MigrationFileItem(
+									contentUrl, title, itemStatus.toString());
+							rv.add(item);
 
-									// catch the BoxAPIException e
-									// and halt the whole upload process
-									break;
+							// finish up and continue to next item
+							continue;
 						}
 					}
 
@@ -883,7 +890,9 @@ class MigrationTaskService {
 						boxFolderIdStack.push("title -- conflict with existing Box folder");
 					} else {
 						// log the exception message
-						log.error(e.getResponse() + " for " + title);
+						String errorMessage = "There is a problem adding folder " + title + " to Box: " + e.getMessage();
+						log.error(errorMessage);
+						itemStatus.append(errorMessage);
 
 						// and throws the exception,
 						// so that the parent function can catch it and stop the
@@ -1085,8 +1094,8 @@ class MigrationTaskService {
 				}
 				else
 				{
-					String errorString = "uploadFile fileName=" + fileName
-							+ e.getResponse();
+					String errorString = "There is a problem uploading file " + fileName
+							+ " to Box: " + e.getMessage();
 					log.error(this + errorString);
 					status.append(errorString + Utils.LINE_BREAK);
 				}
@@ -1205,7 +1214,7 @@ class MigrationTaskService {
 					}
 				}
 			}
-			catch (org.json.JSONException jsonException)
+			catch (JSONException jsonException)
 			{
 				log.error("Problem parsing JSON object based on " + e.getResponse());
 			}
